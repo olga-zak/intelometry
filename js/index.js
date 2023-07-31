2f68fd5e90e75c5d1c0b1d0d924ed2efd840677a
olga-zak <olgaliam@gmail.com>
2023-07-31 16:12:14 +0300

    styles

--- js/index.js
+++ js/index.js
@@ -1,19 +1,14 @@
-// const arrowButton = document.querySelectorAll(".menu-arrow");
-// console.log(arrowButton);
-// arrowButton.forEach((el) =>
-//   el.addEventListener("click", (event) => {
-//     const subMenu = event.target.parentElement.querySelector(".sub-menu");
-//     subMenu.classList.toggle("open");
-//   })
-// );
-
 const arrowButton = document.querySelectorAll(".parent");
-console.log(arrowButton);
 arrowButton.forEach((el) =>
   el.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log(event.target);
     const subMenu = event.target.parentElement.querySelector(".sub-menu");
     subMenu.classList.toggle("open");
   })
 );
+
+const burgerOpenBtn = document.querySelector(".checkbox");
+burgerOpenBtn.addEventListener("click", () => {
+  const mobMenu = document.querySelector(".mobile-menu");
+  mobMenu.classList.toggle("invisible");
+});
