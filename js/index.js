4cd293fdc3b9b304abb721aff8be9c0799349696
olga-zak <olgaliam@gmail.com>
2023-08-03 00:05:27 +0300

    mobile menu styles upd

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 const burgerOpenBtn = document.querySelector(".checkbox");
 const mobMenu = document.querySelector(".navigation");
 const overlay = document.querySelector(".overlay");
+const wrapper = document.querySelector(".wrapper");
+
 burgerOpenBtn.addEventListener("click", () => {
   mobMenu.classList.toggle("open");
   overlay.classList.toggle("open");
+  wrapper.style.overflow = "hidden";
+  if (!overlay.classList.contains("open")) {
+    wrapper.style.overflow = "auto";
+  }
 });
 overlay.addEventListener("click", () => {
   mobMenu.classList.remove("open");
   overlay.classList.remove("open");
+  wrapper.style.overflow = "auto";
 });
 
 const navDopdownMenu = document.querySelectorAll(".dropdown-open-btn");
